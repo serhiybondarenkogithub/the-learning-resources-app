8895aad61f7e9851b5f774530b49afe740386194
serhiybondarenkogithub <sergiogetstudy@gmail.com>
2025-08-01 19:33:34 +0300

    adding a form, fetching user input

--- src/components/learning-resources/AddResource.vue
+++ src/components/learning-resources/AddResource.vue
@@ -1,3 +1,65 @@
 <template>
-  <h2>Add Resource</h2>
+  <base-card>
+    <form @submit.prevent="submitData">
+      <div>
+        <label for="title">Title</label>
+        <input id="title" name="title" type="text" ref="titleInput">
+      </div>
+      <div>
+        <label for="description">Description</label>
+        <input id="description" name="description" type="text" ref="descInput">
+      </div>
+      <div>
+        <label for="link">Link</label>
+        <input id="link" name="link" type="text" ref="linkInput">
+      </div>
+      <div>
+        <base-button type="submit">Submit</base-button>
+      </div>
+    </form>
+  </base-card>
 </template>
+
+<script>
+export default {
+  inject: ["addResource"],
+  methods: {
+    submitData() {
+      console.log(this.$refs.titleInput.value)
+      const enteredTitle = this.$refs.titleInput.value;
+      const enteredDescription = this.$refs.descInput.value;
+      const enteredUrl = this.$refs.linkInput.value;
+
+      this.addResource(enteredTitle, enteredDescription, enteredUrl);
+    }
+  }
+}
+</script>
+
+<style scoped>
+label {
+  font-weight: bold;
+  display: block;
+  margin-bottom: 0.5rem;
+}
+
+input,
+textarea {
+  display: block;
+  width: 100%;
+  font: inherit;
+  padding: 0.15rem;
+  border: 1px solid #ccc;
+}
+
+input:focus,
+textarea:focus {
+  outline: none;
+  border-color: #3a0061;
+  background-color: #f7ebff;
+}
+
+.form-control {
+  margin: 1rem 0;
+}
+</style>
--- src/components/learning-resources/TheResources.vue
+++ src/components/learning-resources/TheResources.vue
@@ -36,7 +36,8 @@ export default {
   },
   provide() {
     return {
-      resources: this.storedResources
+      resources: this.storedResources,
+      addResource: this.addResource
     }
   },
   computed: {
@@ -50,6 +51,16 @@ export default {
   methods: {
     setSelectedTab(tab) {
       this.selectedTab = tab;
+    },
+    addResource(title, description, url) {
+      const newResource = {
+        id: new Date().toISOString(),
+        title: title,
+        description: description,
+        link: url
+      };
+      this.storedResources.unshift(newResource);
+      this.selectedTab = "stored-resources";
     }
   }
 }
