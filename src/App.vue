dffb53352c792b6b0ad3afb4115730eacd7f7a96
serhiybondarenkogithub <sergiogetstudy@gmail.com>
2025-08-01 18:49:48 +0300

    dynamic component $ attribute failthrough, adding $ styling tabs

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,16 @@
 <template>
   <the-header title="RememberMe"></the-header>
-  <stored-resources :resources="storedResources" />
+  <the-resources></the-resources>
 </template>
 
 <script>
 import TheHeader from './components/layout/TheHeader.vue';
-import StoredResources from './components/learning-resources/StoredResources.vue';
-
+import TheResources from './components/learning-resources/TheResources.vue';
 
 export default {
   components: {
     TheHeader,
-    StoredResources
-  },
-  data() {
-    return {
-      storedResources: [
-        { 
-          id: "official-guide", 
-          title: "Official Guide", 
-          description: "The official Vue.js documentation.",
-          link: "https://vuejs.org"
-        },
-        {
-          id: "google",
-          title: "Google",
-          description: "Learn to google...",
-          link: "https://google.com"
-        }
-      ]
-    }
+    TheResources
   }
 }
 </script>
